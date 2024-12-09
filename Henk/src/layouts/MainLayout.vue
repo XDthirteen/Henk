58ca546bbdae363fbf5bef4bc5149a341f3c41b4
Arno Defillet <arnodefillet@hotmail.com>
2024-12-09 22:01:38 +0100

    File structure start

---
+++ Henk/src/layouts/MainLayout.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
+
+</script>
+
+<template>
+    <div>hello world</div>
+    <button @click="router.push({ name: 'login' })">Login</button>
+    <RouterView/>
+</template>
---
+++ Henk/src/views/LoginView.vue
@@ -0,0 +1,7 @@
+<script type="ts" setup>
+console.log('hey')
+</script>
+
+<template>
+    <div>hello HEY</div>
+</template>
