079c31d1b18de4d4b840aad78a5006524c88923d
Arno Defillet <arnodefillet@hotmail.com>
2024-12-16 19:45:22 +0100

    Toevoegen van commentblok + template

--- Henk/src/views/LoginView.vue
+++ Henk/src/views/LoginView.vue
@@ -1,26 +1,58 @@
+#####################################
+*/
+*/      #  LoginVue.vue
+*/      #  ==================
+*/      #  Beschrijving:
+*/      #  ------------
+*/      #  Dit zorgt ervoor dat je kan aanmelden.
+*/ 
+*/      #  Auteur: Arno Defillet
+*/      #  Datum aangemaakt: 11/12/2024
+*/
+##################
+*/
+*/      Changelog:
+*/      ----------
+*/      11/12/2024 - Creatie van view + uitwerken van loginfunctionaliteit.
+*/      16/12/2024 - Toevoegen van comments
+*/      
+*/      Opmerkingen:
+*/      ------------
+*/      ...
+*/      
+#####################################
+
 <script lang="ts" setup>
 import axios from 'axios';
 import { ref, type Ref } from 'vue';
 
+// Declareer een referentie voor de ingevoerde gebruikersnaam & wachtwoord
 const inputUsername: Ref<string> = ref("");
 const inputPassword: Ref<string> = ref("");
 
+// Controleer of er al een authenticatietoken in de lokale opslag staat
+// Hiermee wordt bepaald of de gebruiker al ingelogd is
 const isAuthenticated = ref(localStorage.getItem("token") !== null);
 
+// Functie om de gebruiker in te loggen
 const login = async (email: string, password: string) => {
     try {
+        // Verstuur een POST-verzoek naar de API met de ingevoerde email en wachtwoord
         const response = await axios.post<{token: string}>("/api/auth/login", {
             email: email,
             password: password
         });
+
         const token = response.data.token;
         localStorage.setItem("token", token);
         isAuthenticated.value = true;
+
     } catch (error) {
         throw new Error(`Login wrong. Error: ${error}`);
     }
 }
 
+// Functie om de huidige gebruiker te authenticeren
 const authenticate = async() => {
     try {
         await login(inputUsername.value, inputPassword.value)
