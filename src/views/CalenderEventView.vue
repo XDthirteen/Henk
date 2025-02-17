6609f554d95288525f00e034646422c4bce77885
Gert-Jan Germeys <gert-jan.germeys@hotmail.com>
2025-02-17 21:53:03 +0100

    nog niks gedaan maar rappe switch naar weather

---
+++ src/views/CalenderEventView.vue
@@ -0,0 +1,15 @@
+<script lang="ts">
+
+</script>
+
+
+<template>
+<div class="calender-events">
+  <div>titel event</div>
+  <div>time event</div>
+  <div>note event</div>
+  <div>location event</div>
+  <div>weather predictions event</div>
+  <div>cancel & save event</div>
+</div>
+</template>
