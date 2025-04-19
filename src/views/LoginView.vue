0779533a5feb2591ecc5645314dc9bf63353c7a1
Arno Defillet <arnodefillet@hotmail.com>
2025-04-19 19:11:08 +0200

    Adding enter-key on login

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -70,7 +70,7 @@ const authenticate = async () => {
       <StyledInputByType input-type="email" placeholder="example@example.com" v-model="inputUsername"
         autocomplete="email"></StyledInputByType>
       <StyledInputByType input-type="password" placeholder="Your password" v-model="inputPassword"
-        autocomplete="current-password"></StyledInputByType>
+        autocomplete="current-password" @keyup.enter="authenticate()"></StyledInputByType>
     </form>
 
     <div class="button-container">
