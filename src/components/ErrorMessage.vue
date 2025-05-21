721331fc184cb029073e1d77df0469a88683bc48
Arno Defillet <arnodefillet@hotmail.com>
2025-05-21 21:53:34 +0200

    Fixed tasks after merge + errormessages on blank titles

---
+++ src/components/ErrorMessage.vue
@@ -0,0 +1,17 @@
+<template>
+  <p>
+    <slot />
+  </p>
+</template>
+
+<style scoped>
+p {
+  display: flex;
+  justify-content: center;
+  padding: 0.2rem;
+  border: 0.5rem solid red;
+  border-radius: 10px;
+  background-color: var(--main-red);
+  font-weight: bold;
+}
+</style>
